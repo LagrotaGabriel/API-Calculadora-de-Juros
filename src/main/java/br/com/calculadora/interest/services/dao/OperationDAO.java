fd1ed99cdb969b14d7c6bd74727ccc594a384330
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-05-24 00:06:07 -0300

    -> Agora os Data Access Objects possuem um método de findAll() -> Agora o findById dos DAO lança uma excessão de objeto não encontrado caso não encontre nenhum id paralelo ao enviado via post no banco de dados

--- src/main/java/br/com/calculadora/interest/services/dao/OperationDAO.java
+++ src/main/java/br/com/calculadora/interest/services/dao/OperationDAO.java
@@ -2,7 +2,12 @@ package br.com.calculadora.interest.services.dao;
 
 import br.com.calculadora.interest.models.entities.OperationEntity;
 
+import java.util.List;
+
 public interface OperationDAO {
 
+    List<OperationEntity> findAll();
+
     OperationEntity findById(Long id);
+
 }
--- src/main/java/br/com/calculadora/interest/services/dao/OperationDAOImpl.java
+++ src/main/java/br/com/calculadora/interest/services/dao/OperationDAOImpl.java
@@ -1,10 +1,12 @@
 package br.com.calculadora.interest.services.dao;
 
+import br.com.calculadora.interest.exceptions.ObjectNotFoundException;
 import br.com.calculadora.interest.models.entities.OperationEntity;
 import br.com.calculadora.interest.repositories.OperationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,10 +15,18 @@ public class OperationDAOImpl implements OperationDAO{
     @Autowired
     OperationRepository operationRepository;
 
+
+    @Override
+    public List<OperationEntity> findAll() {
+        return operationRepository.findAll();
+    }
+
     @Override
     public OperationEntity findById(Long id) {
         Optional<OperationEntity> optionalOperationEntity = operationRepository.findById(id);
-        return optionalOperationEntity.orElse(null);
+        return optionalOperationEntity.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
+
+
 
 }
