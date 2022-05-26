e29858090d83e7b3677ba1192031e50857196819
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-05-26 09:05:46 -0300

    -> Dei prosseguimento na lógica do Validation e do Service

--- src/main/java/br/com/calculadora/interest/services/OperationService.java
+++ src/main/java/br/com/calculadora/interest/services/OperationService.java
@@ -1,17 +1,38 @@
 package br.com.calculadora.interest.services;
 
 import br.com.calculadora.interest.models.dto.OperationDTO;
-import br.com.calculadora.interest.models.enums.FetchedParameter;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class OperationService {
 
+    /** Método que realiza conversão do tempo da fórmula de cálculo de juros para dias.
+     * 1 MES -> 30 dias
+     * 1 BIMESTRE -> 60 dias
+     * 1 TRIMESTRE -> 90 dias
+     * 1 SEMESTRE -> 180 dias
+     * 1 ANO -> 360 dias
+     * @param operationDTO -> Recebe um objeto do tipo OperationDTO
+     * @return -> Retorna um valor do tipo inteiro que é a quantidade de dias na qual os juros devem trabalhar */
     public Integer convertTimeToDays(OperationDTO operationDTO){
+
+        /* SE O TEMPO FOR DIFERENTE DE NULO:
+         * Deve ser realizada a conversão do tempo.
+         * SE O TEMPO FOR NULO:
+         * O método irá retornar null */
         if(operationDTO.getTime() != null) return operationDTO.getTimeCategory().getDays() * operationDTO.getTime();
         return null;
+
+    }
+
+    /** Método que realiza o cálculo do montante, seta no objeto DTO e retorna esse total
+     * Fórmula utilizada -> M = C + J (MONTANTE = CAPITAL INICIAL + JUROS TOTAIS)
+     * @param operationDTO -> Recebe como parâmetro um objeto do tipo OperationDTO
+     * @return -> Retorna um valor do tipo double que é o total do montante da operação */
+    public Double amountOperation(OperationDTO operationDTO){
+        Double sum = operationDTO.getApplied() + operationDTO.getInterest();
+        operationDTO.setAmount(sum);
+        return sum;
     }
 
     public void interestOperation(OperationDTO operationDTO){
--- src/main/java/br/com/calculadora/interest/validations/OperationAttributesValidation.java
+++ src/main/java/br/com/calculadora/interest/validations/OperationAttributesValidation.java
@@ -1,8 +1,11 @@
 package br.com.calculadora.interest.validations;
 
 import br.com.calculadora.interest.exceptions.InvalidParametersException;
+import br.com.calculadora.interest.exceptions.NullPointerException;
 import br.com.calculadora.interest.models.dto.OperationDTO;
 import br.com.calculadora.interest.models.enums.FetchedParameter;
+import br.com.calculadora.interest.services.OperationService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Fórmula para calcular montante:
@@ -12,10 +15,45 @@ import br.com.calculadora.interest.models.enums.FetchedParameter;
  * j = c * i * t (JUROS = CAPITAL * TAXA DE JUROS * TEMPO INVESTIDO)
  *
  * Fórmula para calcular juros compostos:
- * m = c * (1 + i) ^ t (MONTANTE = CAPITAL * (1 + TAXA DE JUROS ) ^ TEMPO INVESTIDO) */
+ * m = c * (1 + i) ^ t (MONTANTE = CAPITAL * (1 + TAXA DE JUROS ) ^ TEMPO INVESTIDO)
+ **/
 public class OperationAttributesValidation {
 
-    public static Boolean attributesValidation(OperationDTO operationDTO) {
+    /* Instanciação da classe de processamento da entidade OperationServiceEntity,
+     * que tem como objetivo realizar os cálculos e processamentos do sistema */
+    @Autowired OperationService operationService;
+
+    /** Método que tem como objetivo validar a quantidade de itens da operação que são nulos
+     * Caso mais de um item seja nulo, a operação não pode ser realizada
+     * @param operationDTO -> Recebe como parâmetro um objeto do tipo operationDTO
+     * @return -> Retorna um Boolean com false caso mais de um item seja null e com true caso menos de um item seja null */
+    public Boolean nullAttributesValidation(OperationDTO operationDTO){
+
+        int cont = 0;
+
+        if(operationDTO.getInterest() == null){
+            cont += 1;
+        }
+        else if(operationDTO.getApplied() == null){
+            cont += 1;
+        }
+        else if(operationDTO.getInterestRate() == null){
+            cont += 1;
+        }
+        else if(operationDTO.getTime() == null){
+            cont += 1;
+        }
+
+        if(cont > 1) return false;
+        return true;
+
+    }
+
+    /** Método responsável por realizar as validações de atributos inseridos pelo usuário com base
+     *  no parâmetro da operação na qual o mesmo esteja buscando
+     * @param operationDTO -> Recebe em seu parâmetro um objeto do tipo OperationDTO
+     * @return -> Retorna um Boolean com true se as inserções forem validadas e false se estiverem incorretas */
+    public Boolean attributesValidation(OperationDTO operationDTO) {
 
         /* SE O USUÁRIO QUISER CALCULAR O MONTANTE
          * Caso os JUROS ou o CAPITAL sejam nulos, necessários calculá-los
@@ -27,10 +65,46 @@ public class OperationAttributesValidation {
             if(operationDTO.getAmount() != null){
                 throw new InvalidParametersException("Valor buscado já preenchido na requisição");
             }
+
             /* SE O MONTANTE ESTIVER VAZIO
              * Realizar cálculo do montante */
             else{
-                //TODO VERIFICAR SE JUROS OU CAPITAL SÃO NULOS, PARA REALIZAR CÁLCULOS DOS MESMOS
+
+                /* SE O JUROS E O CAPITAL APLICADO ESTIVEREM PREENCHIDOS
+                 * O cálculo do montante é realizado direto, sem necessidade de cálculos adicionais para chegar ao
+                 * seu resultado*/
+                if(operationDTO.getInterest() != null && operationDTO.getApplied() != null){
+                    operationService.amountOperation(operationDTO);
+                }
+
+                /* SE O JUROS OU O CAPITAL APLICADO NÃO ESTIVEREM PREENCHIDOS
+                 * Será necessário validar quantos campos não estão preenchidos
+                 * Se existir mais de um campo sem preenchimento, não e possível realizar a operação */
+                else{
+
+                    /* SE MAIS DE UM ATRIBUTO DA OPERAÇÃO MATEMÁTICA ESTIVER NULO
+                     * Não é possível prosseguir com a operação, lançando uma operação do tipo NullPointerException */
+                    if(!nullAttributesValidation(operationDTO)){
+                        throw new NullPointerException("Não é possível realizar a operação matemática com mais de um" +
+                                "atributo não definido");
+                    }
+
+                    /* SE APENAS UM ATRIBUTO DA OPERAÇÃO MATEMÁTICA ESTIVER NULO
+                     * Descobrir qual é o valor nulo da operação, e com base nisso descobrir a
+                     * incognita da operação */
+                    else{
+
+                        if(operationDTO.getInterest() == null){
+                            // TODO OPERAÇÃO DE CÁLCULO DE JUROS
+                        }
+                        if(operationDTO.getApplied() == null){
+                            // TODO OPERAÇÃO DE CÁLCULO DE CAPITAL
+                        }
+
+                    }
+
+                }
+
             }
 
         }
