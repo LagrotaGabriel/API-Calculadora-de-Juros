e488810f877555784c2babc2367039ef25a281b6
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-05-27 10:38:08 -0300

    -> Dei prosseguimento na lógica do Validation e do Service [EM CONSTRUÇÃO]

--- src/main/java/br/com/calculadora/interest/services/OperationService.java
+++ src/main/java/br/com/calculadora/interest/services/OperationService.java
@@ -1,8 +1,19 @@
 package br.com.calculadora.interest.services;
 
 import br.com.calculadora.interest.models.dto.OperationDTO;
+import br.com.calculadora.interest.models.enums.InterestType;
 import org.springframework.stereotype.Service;
 
+/**
+ * Fórmula para calcular montante:
+ * m = j + c (MONTANTE = JUROS + CAPITAL)
+ *
+ * Fórmula para calcular juros simples:
+ * j = c * i * t (JUROS = CAPITAL * TAXA DE JUROS * TEMPO INVESTIDO)
+ *
+ * Fórmula para calcular juros compostos:
+ * m = c * (1 + i) ^ t (MONTANTE = CAPITAL * (1 + TAXA DE JUROS ) ^ TEMPO INVESTIDO)
+ **/
 @Service
 public class OperationService {
 
@@ -25,17 +36,44 @@ public class OperationService {
 
     }
 
-    /** Método que realiza o cálculo do montante, seta no objeto DTO e retorna esse total
+    /** Método que realiza o cálculo do montante, seta no objeto DTO e retorna o OperationDTO com o valor calculado
      * Fórmula utilizada -> M = C + J (MONTANTE = CAPITAL INICIAL + JUROS TOTAIS)
      * @param operationDTO -> Recebe como parâmetro um objeto do tipo OperationDTO
-     * @return -> Retorna um valor do tipo double que é o total do montante da operação */
-    public Double amountOperation(OperationDTO operationDTO){
-        Double sum = operationDTO.getApplied() + operationDTO.getInterest();
-        operationDTO.setAmount(sum);
-        return sum;
+     * @return -> Retorna o objeto OperationDTO com o valor do montante atualizado */
+    public OperationDTO amountOperation(OperationDTO operationDTO){
+        operationDTO.setAmount(operationDTO.getApplied() + operationDTO.getInterest());
+        return operationDTO;
     }
 
-    public void interestOperation(OperationDTO operationDTO){
+    /** Método que realiza o cálculo do juros, seta no objeto DTO e retorna o OperationDTO com o valor calculado
+     * Fórmula para juros simples -> J = C * i * t
+     * Fórmula para Juros compostos -> M = C * ((1 + i) ^ t))
+     * @param operationDTO -> Recebe como parâmetro um objeto do tipo OperationDTO
+     * @return Retorna o objeto OperationDTO com o valor dos juros atualizado */
+    public OperationDTO interestOperation(OperationDTO operationDTO){
+
+        /* SE O TIPO DE JUROS BUSCADO PELO USUÁRIO FOR JUROS SIMPLES */
+        if(operationDTO.getInterestType().equals(InterestType.SIMPLE)){
+
+            /* FÓRMULA PARA CÁLCULO DE JUROS SIMPLES */
+            operationDTO.setInterest
+                    (operationDTO.getApplied() * (operationDTO.getInterestRate()/100) * operationDTO.getTime());
+
+        }
+
+        else if(operationDTO.getInterestType().equals(InterestType.COMPOUND)){
+
+            operationDTO.setAmount
+                    (operationDTO.getApplied() * (1 + (Math.pow(operationDTO.getInterestRate()/100, operationDTO.getTime()))));
+
+
+        }
+
+        return null;
+
+    }
+
+    public void appliedOperation(OperationDTO operationDTO){
 
     }
 
