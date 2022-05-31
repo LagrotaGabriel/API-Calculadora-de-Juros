b512ee4e03aaf3c722312988759f5f8246a2ca17
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-05-31 15:45:20 -0300

    -> Otimização e documentação da interface OperationDAO

--- src/main/java/br/com/calculadora/interest/services/dao/OperationDAO.java
+++ src/main/java/br/com/calculadora/interest/services/dao/OperationDAO.java
@@ -5,6 +5,11 @@ import br.com.calculadora.interest.models.entities.OperationEntity;
 
 import java.util.List;
 
+/** Interface utilizada para conter o acesso aos métodos do repositório de acesso ao banco de dados
+ ** @author Gabriel Lagrota
+ ** @version 1.0.0
+ ** @email gabriellagrota23@gmail.com
+ ** @since 31/05/2022 */
 public interface OperationDAO {
 
     List<OperationEntity> findAll();
